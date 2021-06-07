d844d1ce182b0c05d1fbb6f4cefc5340dcc37c1a
mitsumaru3291 <morimitsu8428@gmail.com>
2021-06-07 21:47:32 +0900

    Updated make_movie.py

--- src/make_movie.py
+++ src/make_movie.py
@@ -33,45 +33,43 @@ def calc_cur_hand_pos(l1,l2,l3,phi1,phi2,phi3):
 
     return cur_hand_pos
 
-# 追従させる軌道 直線 端点の2つの座標を与える.  req_tは処理時間. 各時刻における所望の座標のリストを返す(indexは分割数個)
-# 2つの直線がまじったようなものを考える
-def make_orbit(start, middle, end, add_mid, add_end):
+# 直線の軌道を生成 直線 端点の2つの座標を与える. 
+def make_line_trajectory(start, end, req_t):
     cur_t = 0
     x_refs = []
     y_refs = []
-    while(cur_t < add_mid):
-        #s = 6*(cur_t/req_t)**5 - 15*(cur_t/req_t)**4 + 10*(cur_t/req_t)**3
-        s = cur_t/add_mid
-        x_ref = start[0]*(1-s) + middle[0]*s
-        y_ref = start[1]*(1-s) + middle[1]*s
-        x_refs.append(x_ref)
-        y_refs.append(y_ref)
-        cur_t += 1
-
-    cur_t = 0
-    while(cur_t < add_end):
-        s = cur_t/add_mid
-        x_ref = middle[0]*(1-s) + end[0]*s
-        y_ref = middle[1]*(1-s) + end[1]*s
-        x_refs.append(x_ref)
-        y_refs.append(y_ref)
-        cur_t += 1
-    
-    return x_refs, y_refs
-
-# 追従させる軌道 直線 端点の2つの座標を与える.  req_tは処理時間. 各時刻における所望の座標のリストを返す(indexは分割数個)
-def calc_line_coodinate_bytime(start, end, req_t):
-    cur_t = 0
-    x_refs = []
-    y_refs = []
-    while(cur_t < req_t):
-        #s = 6*(cur_t/req_t)**5 - 15*(cur_t/req_t)**4 + 10*(cur_t/req_t)**3
+    while cur_t < req_t:
         s = cur_t/req_t
         x_ref = start[0]*(1-s) + end[0]*s
         y_ref = start[1]*(1-s) + end[1]*s
         x_refs.append(x_ref)
         y_refs.append(y_ref)
         cur_t += 1
+    
+    return x_refs, y_refs
+
+# 円の軌道を生成 始点と角度を与える
+def calc_circle_trajectory(start, theta, req_t):
+    cur_t = 0
+    x_refs = []
+    y_refs = []
+    x1 = start[0]
+    y1 = start[1]
+    x_refs.append(x1)
+    y_refs.append(y1)
+    while cur_t < req_t:
+        # 回転行列を計算
+        r_mat = np.array([[cos(theta/req_t),-sin(theta/req_t)],
+                          [sin(theta/req_t), cos(theta/req_t)]])
+        
+        x = np.array([[x1],
+                      [y1]])
+        
+        x_new = np.dot(r_mat, x)
+        x1 = x_new[0][0]
+        y1 = x_new[1][0]
+        x_refs.append(x1)
+        y_refs.append(y1)
     
     return x_refs, y_refs
 
@@ -86,37 +84,16 @@ if __name__ == "__main__":
     fig = plt.figure()
     flag_legend = True
 
-    # 解析、ヤコビ
+    # params
     req_t = 200
     phi1 = 0.8
     phi2 = -0.1
     phi3 = 0.7
 
-    # cannot
-    req_t = 100
-    phi1 = 0.056246
-    phi2 = -0.751651
-    phi3 = -0.025566
-
     cur_hand_pos = calc_cur_hand_pos(l1,l2,l3,pi/2 - phi1,phi2,phi3)
     start = [cur_hand_pos[0][0], cur_hand_pos[1][0]]
-    end = [cur_hand_pos[0][0], cur_hand_pos[1][0] - 105]
-    x_refs, y_refs = calc_line_coodinate_bytime(start, end, req_t)
-
-    # 冗長ヤコビ
-    """
-    phi1 = 1.8
-    phi2 = 0.5
-    phi3 = -0.5
-
-    cur_hand_pos = calc_cur_hand_pos(l1,l2,l3,phi1,phi2,phi3)
-    start = [cur_hand_pos[0][0], cur_hand_pos[1][0]]
-    middle = [cur_hand_pos[0][0], cur_hand_pos[1][0] -25] # +215 +5
-    end = [cur_hand_pos[0][0], cur_hand_pos[1][0] -50] # +220 -150
-    add_mid = 25
-    add_end = 25
-    x_refs, y_refs = make_orbit(start, middle, end, add_mid, add_end)
-    """
+    end = [cur_hand_pos[0][0], cur_hand_pos[1][0] - 100]
+    x_refs, y_refs = make_line_trajectory(start, end, req_t) # 直線軌道を生成
 
     f = open('analytics/ik/last_ik.txt', 'r')
     datalist = f.readlines()
@@ -132,12 +109,8 @@ if __name__ == "__main__":
 
         if flag_legend: # 一回のみ凡例を描画
             plt.plot(x_refs,y_refs,label="target trajectory")
-            #plt.xlim(-170,170) # 冗長ヤコビ
-            #plt.ylim(-30,350) # 冗長ヤコビ
-            #plt.xlim(-70,230) # 解析、ヤコビ
-            #plt.ylim(-30,270) # 解析、ヤコビ
-            plt.xlim(-50,250) # cannot
-            plt.ylim(-30,300) # cannot
+            plt.xlim(-50,250)
+            plt.ylim(-30,300)
             plt.axes().set_aspect('equal')
             plt.legend(loc='upper left')
             plt.title('Analytical Solution',loc='center')
